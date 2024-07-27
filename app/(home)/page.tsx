8f443947210850842c8baa8c4d60d583a88baa3a
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-28 04:13:16 +0900

    Feat: home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,14 @@
+import BestSellerLists from '../components/BestSellerLists';
+
 export const metadata = {
   title: 'Home',
 };
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>THE NEW YORK TIMES BEST SELLER EXPLORER</h1>
+      <BestSellerLists />
+    </div>
+  );
 }
