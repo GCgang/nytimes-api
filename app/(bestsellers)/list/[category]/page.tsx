de2caff917057d879e8b7e90315917a66f998e8f
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-28 05:46:59 +0900

    Feat: BookLists

---
+++ app/(bestsellers)/list/[category]/page.tsx
@@ -0,0 +1,18 @@
+import CategoryBooks from '../../../components/CategoryBooks';
+
+interface IParams {
+  params: { category: string };
+}
+
+export const metadata = {
+  title: 'BookLists',
+};
+
+export default async function BookLists({ params: { category } }: IParams) {
+  return (
+    <div>
+      <h1>{decodeURIComponent(category)}</h1>
+      <CategoryBooks category={category} />
+    </div>
+  );
+}
--- app/(bestsellers)/list/[id]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function BookList() {
-  return <h1>Book List</h1>;
-}
