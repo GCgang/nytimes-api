8e20c3d73b8a16cc8257ad146025e033ecf9fd9a
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-28 10:39:50 +0900

    Feat: not-found, error

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Not Found',
+};
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Error</h1>
+      <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
+      <p>홈페이지로 이동해 주세요.</p>
+      <Link href={'/'}>Home</Link>
+    </div>
+  );
+}
