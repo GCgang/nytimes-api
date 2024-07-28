99d3b07a08460de566d0596254cf22a3a2f7c489
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-07-29 02:11:04 +0900

    Feat: CSS-Error

--- app/(bestsellers)/list/[category]/error.tsx
+++ app/(bestsellers)/list/[category]/error.tsx
@@ -1,14 +1,18 @@
 'use client';
-
+import styles from '../../../../styles/NotFound.module.css';
 import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Error</h1>
-      <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
-      <p>홈페이지로 이동해 주세요.</p>
-      <Link href={'/'}>Home</Link>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Error</h1>
+      <p className={styles.description}>
+        죄송합니다. 페이지를 찾을 수 없습니다.
+      </p>
+      <p className={styles.description}>홈페이지로 이동해 주세요.</p>
+      <Link className={styles.button} href={'/'}>
+        Home
+      </Link>
     </div>
   );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,4 @@
+import styles from '../styles/NotFound.module.css';
 import { Metadata } from 'next';
 import Link from 'next/link';
 
@@ -7,11 +8,15 @@ export const metadata: Metadata = {
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Error</h1>
-      <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
-      <p>홈페이지로 이동해 주세요.</p>
-      <Link href={'/'}>Home</Link>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Error</h1>
+      <p className={styles.description}>
+        죄송합니다. 페이지를 찾을 수 없습니다.
+      </p>
+      <p className={styles.description}>홈페이지로 이동해 주세요.</p>
+      <Link className={styles.button} href={'/'}>
+        Home
+      </Link>
     </div>
   );
 }
